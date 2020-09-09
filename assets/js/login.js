04f1a04caf7acc2ee98555c9fdbc58c4b8e34325
Sakura <1275844708@qq.com>
2020-09-09 17:16:04 +0800

    登录注册验证规则完成

--- assets/js/login.js
+++ assets/js/login.js
@@ -8,4 +8,18 @@ $(function () {
     $('.login-box').hide()
 
   })
+
+
+  var form = layui.form
+
+  form.verify({
+    pwd:[/^[\S]{6,12}$/,'密码必须6-12位，且不能出现空格'],
+
+    repwd:function(value){
+      var pwd = $('.reg_box[name=password]').val()
+      if (pwd !== value) {
+        return '两次密码不一致'
+      }
+    }
+  })
 })
