6814d0bc2d3f9d8fe788ba35c6bb7d0340ca0d67
Sakura <1275844708@qq.com>
2020-09-11 11:15:36 +0800

    注册登录模块完成

--- assets/js/login.js
+++ assets/js/login.js
@@ -1,9 +1,9 @@
 $(function () {
-  $('#login').on('click',function(){
+  $('#login').on('click', function () {
     $('.login-box').show()
     $('.reg-box').hide()
   })
-  $('#reg').on('click',function(){
+  $('#reg').on('click', function () {
     $('.reg-box').show()
     $('.login-box').hide()
 
@@ -11,15 +11,59 @@ $(function () {
 
 
   var form = layui.form
-
+  var layer = layui.layer;
+  // 表单验证
   form.verify({
-    pwd:[/^[\S]{6,12}$/,'密码必须6-12位，且不能出现空格'],
+    pwd: [/^[\S]{6,12}$/, '密码必须6-12位，且不能出现空格'],
 
-    repwd:function(value){
-      var pwd = $('.reg_box[name=password]').val()
-      if (pwd !== value) {
+    repwd: function (value) {
+      var ppwd = $('.reg-box [name=password]').val()
+      if (ppwd != value) {
         return '两次密码不一致'
       }
     }
+  })
+
+  var data = {
+    username: $('.reg-box [name=username]').val(),
+    password: $('.reg-box [name=password]').val()
+  }
+  // var link = 'http://ajax.frontend.itheima.net'
+  // 调用注册接口
+  $('.reg-box').on('submit', function (e) {
+    // 禁用默认行为
+    e.preventDefault()
+    $.post('/api/reguser', data, function (res) {
+      if (res.status !== 0) {
+        return layer.msg(res.message);
+      }
+      layer.msg(res.message);
+      $('#login').click()
+
+    })
+
+  })
+
+
+  // 调用登录接口
+  $('.login-box').on('submit', function (e) {
+    e.preventDefault()
+    $.ajax({
+      url: '/api/login',
+      method: 'post',
+      data: $(this).serialize(),
+      success: function (res) {
+        // console.log(res.status);
+        if (res.status !== 0) {
+          return layer.msg('登录失败');
+        }
+        layer.msg('登陆成功');
+        // console.log(res);
+        localStorage.setItem('token',res.token)
+        location.href = '/index.html'
+
+      }
+    })
+
   })
 })
