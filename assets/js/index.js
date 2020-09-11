6bb2d800032dad60989f9333f9b509c2b5611fd6
Sakura <1275844708@qq.com>
2020-09-11 19:40:56 +0800

    退出功能完毕

---
+++ assets/js/index.js
@@ -0,0 +1,50 @@
+$(function(){
+
+  // 获取用户基本信息
+  getUserInfo()
+
+  function getUserInfo(){
+    $.ajax({
+      method:'GET',
+      url:'/my/userinfo',
+
+      success:function(res){
+        // console.log(res);
+        if (res.status != 0) {
+          return layui.layer.msg('获取用户信息失败')
+        }
+        renderAvatar(res.data)
+      }
+    })
+  }
+
+  $('.logout').on('click',function(){
+    layer.confirm('确定要退出吗?', {icon: 3, title:'提示'}, function(index){
+      //清除token
+      localStorage.removeItem('token')
+      // 转跳到登录页面
+      location.href = '/login.html'
+      layer.close(index);
+    });
+  })
+
+})
+
+
+// 渲染用户头像
+function renderAvatar(user){
+  console.log(user);
+  // 判断是否有昵称,如果没有的话就显示注册时的用户名
+  var name = user.nickname || user.username;
+  $('.welcome').html('欢迎&nbsp' + name)
+
+  // 判断是否有头像
+  if (user.user_pic !== null) {
+    $('.layui-nav-img').attr('url',user.user_pic).show()
+    $('.usertx').hide()
+  }else{
+    var first = name[0].toUpperCase()
+    $('.usertx').html(first).show()
+    $('.layui-nav-img').hide()
+  }
+}
